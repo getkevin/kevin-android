8b64e6850772ced38bc2bde24873a19afd8a746a
Aurimas Žarskis <52950384+aurimas-zarskis@users.noreply.github.com>
2021-06-23 16:41:17 +0300

    MOBILE-76: Add a possibility to pass a locale to the SDK (#7)

--- core/src/main/java/eu/kevin/core/plugin/Kevin.kt
+++ core/src/main/java/eu/kevin/core/plugin/Kevin.kt
@@ -1,16 +1,28 @@
 package eu.kevin.core.plugin
 
+import java.util.*
 import java.util.concurrent.atomic.AtomicBoolean
 
 object Kevin {
 
     private val configurationLock = AtomicBoolean(false)
     private val plugins = mutableSetOf<KevinPlugin>()
+    private var locale: Locale? = null
 
     fun addPlugin(plugin: KevinPlugin) {
         synchronized(this) {
             plugins.add(plugin)
         }
+    }
+
+    fun setLocale(locale: Locale?) {
+        synchronized(this) {
+            this.locale = locale
+        }
+    }
+
+    fun getLocale(): Locale? {
+        return locale
     }
 
     fun configure(configuration: KevinConfiguration) {
--- demo/src/main/java/eu/kevin/demo/DemoApplication.kt
+++ demo/src/main/java/eu/kevin/demo/DemoApplication.kt
@@ -5,12 +5,14 @@ import eu.kevin.accounts.KevinAccountsPlugin
 import eu.kevin.core.plugin.Kevin
 import eu.kevin.core.plugin.KevinConfiguration
 import eu.kevin.inapppayments.KevinPaymentsPlugin
+import java.util.*
 
 class DemoApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
 
+        Kevin.setLocale(Locale("en"))
         Kevin.addPlugin(KevinAccountsPlugin)
         Kevin.addPlugin(KevinPaymentsPlugin)
         Kevin.configure(
