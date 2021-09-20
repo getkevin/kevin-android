0a41e7415b9c094a1a7cdb708dd50c7d37556eb1
Aurimas Žarskis <52950384+aurimas-zarskis@users.noreply.github.com>
2021-09-20 11:02:13 +0300

    MOBILE-219: Handle PaymentSessionContract edge cases (#31)

--- in-app-payments/src/main/java/eu/kevin/inapppayments/paymentsession/PaymentSessionContract.kt
+++ in-app-payments/src/main/java/eu/kevin/inapppayments/paymentsession/PaymentSessionContract.kt
@@ -1,5 +1,6 @@
 package eu.kevin.inapppayments.paymentsession
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import androidx.activity.result.contract.ActivityResultContract
@@ -20,7 +21,15 @@ class PaymentSessionContract : ActivityResultContract<PaymentSessionConfiguratio
     }
 
     override fun parseResult(resultCode: Int, result: Intent?) : SessionResult<PaymentSessionResult> {
-        return result?.extras?.getParcelable(RESULT_KEY)!!
+        return if (resultCode == Activity.RESULT_OK) {
+            try {
+                result?.extras?.getParcelable(RESULT_KEY)!!
+            } catch (error: Exception) {
+                SessionResult.Failure(error)
+            }
+        } else {
+            SessionResult.Failure(UnknownError("Activity returned unknown resultCode"))
+        }
     }
 
     companion object {
