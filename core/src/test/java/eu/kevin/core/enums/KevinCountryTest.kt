598f6313bd926096d0a0b8f627ab32a5492a2fa7
MichalDrabik <michal.drabik0@gmail.com>
2022-04-08 09:34:41 +0100

    Reformat to project's setup

--- core/src/test/java/eu/kevin/core/enums/KevinCountryTest.kt
+++ core/src/test/java/eu/kevin/core/enums/KevinCountryTest.kt
@@ -8,33 +8,33 @@ import org.junit.Test
 @ExperimentalCoroutinesApi
 class KevinCountryTest : BaseUnitTest() {
 
-  @Test
-  fun `test parse() with null input`() {
-    Assert.assertNull(KevinCountry.parse(null))
-  }
+    @Test
+    fun `test parse() with null input`() {
+        Assert.assertNull(KevinCountry.parse(null))
+    }
 
-  @Test
-  fun `test parse() with empty input`() {
-    Assert.assertNull(KevinCountry.parse(""))
-  }
+    @Test
+    fun `test parse() with empty input`() {
+        Assert.assertNull(KevinCountry.parse(""))
+    }
 
-  @Test
-  fun `test parse() with unknown input`() {
-    Assert.assertNull(KevinCountry.parse("xxx"))
-  }
+    @Test
+    fun `test parse() with unknown input`() {
+        Assert.assertNull(KevinCountry.parse("xxx"))
+    }
 
-  @Test
-  fun `test parse() with uppercase input`() {
-    Assert.assertEquals(KevinCountry.POLAND, KevinCountry.parse("PL"))
-  }
+    @Test
+    fun `test parse() with uppercase input`() {
+        Assert.assertEquals(KevinCountry.POLAND, KevinCountry.parse("PL"))
+    }
 
-  @Test
-  fun `test parse() with lowercase input`() {
-    Assert.assertEquals(KevinCountry.POLAND, KevinCountry.parse("pl"))
-  }
+    @Test
+    fun `test parse() with lowercase input`() {
+        Assert.assertEquals(KevinCountry.POLAND, KevinCountry.parse("pl"))
+    }
 
-  @Test
-  fun `test parse() with mix input`() {
-    Assert.assertEquals(KevinCountry.POLAND, KevinCountry.parse("Pl"))
-  }
+    @Test
+    fun `test parse() with mix input`() {
+        Assert.assertEquals(KevinCountry.POLAND, KevinCountry.parse("Pl"))
+    }
 }
